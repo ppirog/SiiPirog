982fa5fa4bb558fa513b748ce2cbad01e8170319
pawel <pawelpirog99@gmail.com>
2025-05-14 12:24:14 +0200

    Initial commit

---
+++ src/main/java/com/sii/siipirog/SiiPirogApplication.java
@@ -0,0 +1,13 @@
+package com.sii.siipirog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiPirogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiPirogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sii/siipirog/SiiPirogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sii.siipirog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiPirogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
